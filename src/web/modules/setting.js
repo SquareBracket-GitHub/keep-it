63366c2005ddad7fdf622f4ce9e1e0186ed1c201
pixel-punk-github <eomtaegyun08@gmail.com>
2026-06-15 16:27:57 +0900

    still fixing

---
+++ src/web/modules/setting.js
@@ -0,0 +1,12 @@
+// const fs = require("fs");
+
+// async function readSettings() {
+//     const path = `../../../config/settings.json`;
+//     const settings = await JSON.parse(fs.readFileSync(path));
+
+//     return settings;
+// }
+
+// module.exports = {
+//     readSettings
+// }
--- src/web/script.js
+++ src/web/script.js
@@ -1,5 +1,6 @@
 const { getCurrentDate, displayTime } = require('./modules/date');
 const { saveFile } = require('./modules/save');
+const { readSettings } = require('./modules/setting');
 
 displayTime(document);
 
@@ -10,6 +11,8 @@ document.getElementById('closeBtn').addEventListener('click', () => {
 document.getElementById('saveBtn').addEventListener('click', async () => {
     const content = document.querySelector('.content-area').value;
     const filename = `keepit_${getCurrentDate().replace(/[: ]/g, '_')}`;
+
+    content = await readSettings();
 
     saveFile(filename, content);
 });
